fa69b5b68c19e2d0e83649e880680491ae074e0b
KoyomiHidaka <abbishev@mail.ru>
2024-07-03 18:17:43 +0500

    Added Comments

--- BotW/BotW.cpp
+++ BotW/BotW.cpp
@@ -27,90 +27,98 @@ void startBreak(Bot& bot, int64_t chatId) {
     bot.getApi().sendMessage(chatId, "Перерыв закончился. Возобновите работу командой 'Начать'.");
     onBreak = false;
 
-int main()
-{
-    int startingi = 1;
-    Bot bot("7203022991:AAHgQgzs7g0scjPS1zX2xAzL_ZQpTwsie5Q");
-    bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
-        ReplyKeyboardMarkup::Ptr keyboard(new ReplyKeyboardMarkup);
-        KeyboardButton::Ptr button1(new KeyboardButton);
-        button1->text = "Начать";
-        KeyboardButton::Ptr button2(new KeyboardButton);
-        button2->text = "Взять перерыв";
-        keyboard->keyboard.push_back({ button1, button2 });
-        keyboard->oneTimeKeyboard = true;
-        bot.getApi().sendMessage(message->chat->id, "Кнопка начать - начнет отчет проведенного времени работы\n Кнопка стоп - Приостановит время и начнет новый таймер отдыха", false, 0, keyboard);
+    int main()
+    {
+        //Начало кода
+        int startingi = 1;
+        Bot bot("7203022991:AAHgQgzs7g0scjPS1zX2xAzL_ZQpTwsie5Q");
+        bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+            ReplyKeyboardMarkup::Ptr keyboard(new ReplyKeyboardMarkup);
+            KeyboardButton::Ptr button1(new KeyboardButton);
+            button1->text = "Начать";
+            KeyboardButton::Ptr button2(new KeyboardButton);
+            button2->text = "Взять перерыв";
+            keyboard->keyboard.push_back({ button1, button2 });
+            keyboard->oneTimeKeyboard = true;
+            bot.getApi().sendMessage(message->chat->id, "Кнопка начать - начнет отчет проведенного времени работы\n Кнопка стоп - Приостановит время и начнет новый таймер отдыха", false, 0, keyboard);
 
-        });
+            });
 
-
-    int64_t lastUpdateId = 0;
-    vector<Update::Ptr> updates = bot.getApi().getUpdates();
-    for (const auto& update : updates) {
-        if (update->updateId > lastUpdateId) {
-            lastUpdateId = update->updateId;
-        }
-    }
-
-    bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
-        if (message->text == "Начать") {
-            if (!isWorking && !onBreak) {
-                workStart = std::chrono::system_clock::now();
-                isWorking = true;
-                bot.getApi().sendMessage(message->chat->id, "Работа началась.");
+        //Обновление бота
+        int64_t lastUpdateId = 0;
+        vector<Update::Ptr> updates = bot.getApi().getUpdates();
+        for (const auto& update : updates) {
+            if (update->updateId > lastUpdateId) {
+                lastUpdateId = update->updateId;
             }
         }
-        else if (message->text == "Взять перерыв") {
-            if (isWorking) {
-                auto now = std::chrono::system_clock::now();
-                totalWorkTime += std::chrono::duration_cast<std::chrono::milliseconds>(now - workStart);
-                isWorking = false;
-                onBreak = true;
-                breakStart = now;
-                bot.getApi().sendMessage(message->chat->id, "Перерыв начался.");
-
-                // Запускаем отдельный поток для отсчета времени перерыва
-                std::thread(startBreak, std::ref(bot), message->chat->id).detach();
+        //Таймер работы и отдыха
+        bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
+            if (message->text == "Начать") {
+                if (!isWorking && !onBreak) {
+                    workStart = std::chrono::system_clock::now();
+                    isWorking = true;
+                    bot.getApi().sendMessage(message->chat->id, "Работа началась.");
+                }
             }
-        }
-        });
+            else if (message->text == "Взять перерыв") {
+                if (isWorking) {
+                    auto now = std::chrono::system_clock::now();
+                    totalWorkTime += std::chrono::duration_cast<std::chrono::milliseconds>(now - workStart);
+                    isWorking = false;
+                    onBreak = true;
+                    breakStart = now;
+                    bot.getApi().sendMessage(message->chat->id, "Перерыв начался.");
+
+                    // Запускаем отдельный поток для отсчета времени перерыва
+                    std::thread(startBreak, std::ref(bot), message->chat->id).detach();
+                }
+            }
+            });
 
 
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        if (message->text == "Начать") {
-            if (!isWorking && !onBreak) {
-                workStart = std::chrono::system_clock::now();
-                isWorking = true;
-                bot.getApi().sendMessage(message->chat->id, "Работа началась.");
+        bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+
+            if (message->text == "Начать") {
+                if (!isWorking && !onBreak) {
+                    workStart = std::chrono::system_clock::now();
+                    isWorking = true;
+                    bot.getApi().sendMessage(message->chat->id, "Работа началась.");
+                }
             }
-        }
-        else if (message->text == "Взять перерыв") {
-            if (isWorking) {
-                auto now = std::chrono::system_clock::now();
-                totalWorkTime += std::chrono::duration_cast<std::chrono::milliseconds>(now - workStart);
-                isWorking = false;
-                onBreak = true;
-                breakStart = now;
-                bot.getApi().sendMessage(message->chat->id, "Перерыв начался.");
-                std::this_thread::sleep_for(std::chrono::hours(1));
-                bot.getApi().sendMessage(message->chat->id, "Перерыв закончился. Возобновите работу командой 'Начать'.");
-                onBreak = false;
+            else if (message->text == "Взять перерыв") {
+                if (isWorking) {
+                    auto now = std::chrono::system_clock::now();
+                    totalWorkTime += std::chrono::duration_cast<std::chrono::milliseconds>(now - workStart);
+                    isWorking = false;
+                    onBreak = true;
+                    breakStart = now;
+                    bot.getApi().sendMessage(message->chat->id, "Перерыв начался.");
+
+                    std::this_thread::sleep_for(std::chrono::hours(1));
+                    bot.getApi().sendMessage(message->chat->id, "Перерыв закончился. Возобновите работу командой 'Начать'.");
+                    onBreak = false;
+                }
             }
-        }
+            });
+
+
+        //Команда стоп только для админа
+        bot.getEvents().onCommand("stop", [&bot, &startingi](Message::Ptr message) {
+            if (message->from->id == adminid) {
+                bot.getApi().sendMessage(message->chat->id, "Программа приостановила свою работу...!");
+                system("exit");
+                startingi = 2;
+            }
+            else {
+                bot.getApi().sendMessage(message->chat->id, "У вас недостаточно прав для совершения данной операции");
+            }
+
+            });
+        //Обновление и запуск бота
+
     });
-    
 
-
-    bot.getEvents().onCommand("stop", [&bot, &startingi](Message::Ptr message) {
-        if (message->from->id == adminid) {
-            bot.getApi().sendMessage(message->chat->id, "Программа приостановила свою работу...!");
-            system("exit");
-            startingi = 2;
-        }
-        else {
-            bot.getApi().sendMessage(message->chat->id, "У вас недостаточно прав для совершения данной операции");
-        }
-    });
 
     try {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
@@ -132,11 +140,3 @@ int main()
     }
     return 0;
 }
-
-
-
-
-
-
-
-
